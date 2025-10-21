5e28d5a7151300f15c1b5fb5bb6b090b1fb995e7
Rubeny94 <44436716+rubenyanes94@users.noreply.github.com>
2025-10-21 00:23:51 +0000

    [Add my domain generator with a couple of hack domain]

---
+++ app.js
@@ -0,0 +1,32 @@
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let domain = [".com", ".net", ".us",".io"]
+
+for(let i = 0; i < pronoun.length;i++){
+    for(let j = 0; j < adj.length;j++){
+        for(let k = 0; k < noun.length; k++){
+
+            let dominioOriginal = pronoun[i] + adj[i] + noun[k];
+            let nounActual = noun[k];
+
+            for(let l = 0; l < domain.length; l++){
+
+                let domainActual = domain[l];
+
+                console.log(dominioOriginal + domainActual);
+
+                if(nounActual === "racoon" || nounActual === "jogger"){
+                    let domainCut = Math.floor(nounActual.length / 2);
+                    let cut = dominioOriginal.slice(0,dominioOriginal.length - nounActual.length + domainCut)
+                    let cutTwo= nounActual.slice(domainCut);
+
+                    let domainHacked = cut + "." + cutTwo;
+
+                    console.log(domainHacked);
+                }
+            };
+        };
+    };
+};
+
